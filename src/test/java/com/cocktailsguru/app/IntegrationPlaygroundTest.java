c4d86ea91d282c97a2cf37a111c5aa6430cf62c5
Ivan Martos <martos.ivan@slsp.sk>
2017-11-21 15:33:47 +0100

    kotlin maybe?

---
+++ src/test/java/com/cocktailsguru/app/IntegrationPlaygroundTest.java
@@ -0,0 +1,8 @@
+package com.cocktailsguru.app;
+
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {IntegrationTestApp.class})
+public class IntegrationPlaygroundTest {
+}
