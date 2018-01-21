f161a179c98c477e9115146ab74ba07ce1f945ca
Ivan Martos <martos.ivan@gmail.com>
2018-01-21 12:13:49 +0100

    added cocktail object type enum

---
+++ src/main/java/com/cocktailsguru/app/cocktail/domain/CocktailObjectType.kt
@@ -0,0 +1,11 @@
+package com.cocktailsguru.app.cocktail.domain
+
+enum class CocktailObjectType {
+    NONE,
+    COCKTAIL,
+    INGREDIENT,
+    USER_PICTURE,
+    USER_COMMENT_INGREDIENT,
+    USER_COMMENT_PICTURE,
+    USER_COMMENT_COCKTAIL
+}
