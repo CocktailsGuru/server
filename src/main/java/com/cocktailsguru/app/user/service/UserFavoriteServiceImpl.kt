19371d308afb8ef4907f5e498e4b61cf518eca27
Ivan Martos <martos.ivan@gmail.com>
2018-01-21 12:15:26 +0100

    user favorite repository, service and service test

---
+++ src/main/java/com/cocktailsguru/app/user/service/UserFavoriteServiceImpl.kt
@@ -0,0 +1,16 @@
+package com.cocktailsguru.app.user.service
+
+import com.cocktailsguru.app.cocktail.domain.CocktailObjectType
+import com.cocktailsguru.app.user.domain.UserFavorite
+import com.cocktailsguru.app.user.repository.UserFavoriteRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class UserFavoriteServiceImpl @Autowired constructor(private val userFavoriteRepository: UserFavoriteRepository) : UserFavoriteService {
+
+    override fun getFavoriteObjects(objectType: CocktailObjectType, id: Long): List<UserFavorite> {
+        return userFavoriteRepository.findByObjectTypeAndObjectForeignKey(objectType, id)
+    }
+
+}
---
+++ src/test/java/com/cocktailsguru/app/user/service/UserFavoriteServiceImplTest.kt
@@ -0,0 +1,49 @@
+package com.cocktailsguru.app.user.service
+
+import com.cocktailsguru.app.cocktail.domain.CocktailObjectType
+import com.cocktailsguru.app.user.domain.UserFavorite
+import com.cocktailsguru.app.user.repository.UserFavoriteRepository
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@RunWith(MockitoJUnitRunner::class)
+class UserFavoriteServiceImplTest {
+    private val anyUserFavoriteRepository = mock<UserFavoriteRepository>()
+
+    private val userFavoriteService: UserFavoriteService = UserFavoriteServiceImpl(anyUserFavoriteRepository)
+
+    @Test
+    fun givenUserFavoritePresentWhenRequestingUserFavoriteShouldReturnOneItem() {
+        val anyObject = mock<CocktailObjectType>()
+        val anyId = 123L
+        val anyUserFavorite = mock<UserFavorite>()
+        whenever(anyUserFavoriteRepository.findByObjectTypeAndObjectForeignKey(anyObject, anyId)).thenReturn(listOf(anyUserFavorite))
+
+        val favoriteObjects = userFavoriteService.getFavoriteObjects(anyObject, anyId)
+
+        assertFalse(favoriteObjects.isEmpty())
+        assertEquals(1, favoriteObjects.size)
+        assertEquals(anyUserFavorite, favoriteObjects.get(0))
+        verify(anyUserFavoriteRepository).findByObjectTypeAndObjectForeignKey(anyObject, anyId)
+    }
+
+
+    @Test
+    fun giveNoUserFavoriteInRepositoryWhenRequestingUserFavoriteShouldReturnEmptyList() {
+        val anyObject = mock<CocktailObjectType>()
+        val anyId = 123L
+        whenever(anyUserFavoriteRepository.findByObjectTypeAndObjectForeignKey(anyObject, anyId)).thenReturn(listOf())
+
+        val favoriteObjects = userFavoriteService.getFavoriteObjects(anyObject, anyId)
+
+        assertTrue(favoriteObjects.isEmpty())
+        verify(anyUserFavoriteRepository).findByObjectTypeAndObjectForeignKey(anyObject, anyId)
+    }
+}
