259ff98177d677260a1ecb4c6317966cc76e6e46
Ivan Martos <martos.ivan@gmail.com>
2018-01-27 14:18:22 +0100

    paging info has additional constructor

--- src/main/java/com/cocktailsguru/app/common/domain/PagingInfo.kt
+++ src/main/java/com/cocktailsguru/app/common/domain/PagingInfo.kt
@@ -1,6 +1,10 @@
 package com.cocktailsguru.app.common.domain
 
+import com.cocktailsguru.app.common.dto.PagingDto
+
 data class PagingInfo(
         val pageNumber: Int,
         val pageSize: Int
-)
+) {
+    constructor(pagingDto: PagingDto) : this(pagingDto.pageNumber, pagingDto.pageSize)
+}
