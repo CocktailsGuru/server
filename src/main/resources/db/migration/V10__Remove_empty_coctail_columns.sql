819b4e5e57705b7b22a0a19cb9ba30049868c850
Ivan Martos <martos.ivan@gmail.com>
2018-01-27 18:47:49 +0100

    removed empty columns from coctail table

---
+++ src/main/resources/db/migration/V10__Remove_empty_coctail_columns.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `cocktail_dev`.`coctail`
+DROP COLUMN `kcalVolume`,
+DROP COLUMN `flavorId`,
+DROP COLUMN `flavor`,
+DROP COLUMN `dataCol`,
+DROP COLUMN `groupType`,
+DROP COLUMN `groupType2`,
+DROP COLUMN `groupType3`;
