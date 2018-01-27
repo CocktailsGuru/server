0609b3fd2a1e30ac19ea8ccd2f8b0be773a78933
Ivan Martos <martos.ivan@gmail.com>
2018-01-27 19:11:05 +0100

    removed another empty column

--- src/main/resources/db/migration/V10__Remove_empty_coctail_columns.sql
+++ src/main/resources/db/migration/V10__Remove_empty_coctail_columns.sql
@@ -3,6 +3,7 @@ DROP COLUMN `kcalVolume`,
 DROP COLUMN `flavorId`,
 DROP COLUMN `flavor`,
 DROP COLUMN `dataCol`,
+DROP COLUMN `numPrepared`,
 DROP COLUMN `groupType`,
 DROP COLUMN `groupType2`,
 DROP COLUMN `groupType3`;
