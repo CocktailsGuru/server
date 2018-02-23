8bd2e6d65ba9bfdea3856fba5d5442c0693621b7
Ivan Martos <martos.ivan@gmail.com>
2018-02-23 07:38:53 +0100

    modified domain objects and dtos for new ingredients structure

--- src/main/java/com/cocktailsguru/app/ingredient/domain/IngredientList.kt
+++ src/main/java/com/cocktailsguru/app/ingredient/domain/IngredientList.kt
@@ -2,7 +2,7 @@ package com.cocktailsguru.app.ingredient.domain
 
 import com.cocktailsguru.app.common.domain.PagingInfo
 
-data class IngredientList<out T : BaseIngredient>(
-        val list: List<T>,
+data class IngredientList(
+        val list: List<Ingredient>,
         val pagingInfo: PagingInfo
 )
--- src/main/java/com/cocktailsguru/app/ingredient/dto/list/NonAlcoIngredientListItemDto.kt
+++
@@ -1,7 +0,0 @@
-package com.cocktailsguru.app.ingredient.dto.list
-
-data class NonAlcoIngredientListItemDto(
-        val id: Long,
-        val imageName: String,
-        val nameGrouped: String
-)
