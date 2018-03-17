cf932a6b2485e78d3866bba44d0ad094900644d8
Ivan Martos <martos.ivan@gmail.com>
2018-03-17 11:07:15 +0100

    removed custom health

--- src/test/java/com/cocktailsguru/app/IntegrationPlaygroundTest.java
+++ src/test/java/com/cocktailsguru/app/IntegrationPlaygroundTest.java
@@ -1,29 +1,24 @@
 package com.cocktailsguru.app;
 
-import com.cocktailsguru.app.health.controller.HealthController;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {IntegrationTestApp.class})
 @WebAppConfiguration
 @Slf4j
 @Transactional
+@Ignore
 public class IntegrationPlaygroundTest {
 
     private MockMvc mockMvc;
@@ -36,17 +31,5 @@ public class IntegrationPlaygroundTest {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac)
                 .build();
     }
-
-
-    @Test
-    public void shouldGetHealthResponse() throws Exception {
-        MvcResult result = mockMvc.perform(
-                get(HealthController.HEALTH_PATH)
-                        .contentType(APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andReturn();
-        log.info(result.getResponse().getContentAsString());
-    }
-
 
 }
