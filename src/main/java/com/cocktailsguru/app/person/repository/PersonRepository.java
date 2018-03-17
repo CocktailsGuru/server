9144e8dd50a672b768e0581768d3b7d0b58ed1d3
Ivan Martos <martos.ivan@gmail.com>
2018-03-17 12:20:20 +0100

    removed person stuff

--- src/main/java/com/cocktailsguru/app/person/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cocktailsguru.app.person.repository;
-
-import com.cocktailsguru.app.person.domain.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
