21cd0781bb91667429d77928cb4a6c7a646f1700
Ivan Martos <martos.ivan@gmail.com>
2018-03-24 10:48:41 +0100

    removed cache control

--- src/main/java/com/cocktailsguru/app/staticfiles/CocktailsWebMvcConfiguration.kt
+++ src/main/java/com/cocktailsguru/app/staticfiles/CocktailsWebMvcConfiguration.kt
@@ -18,6 +18,7 @@ open class CocktailsWebMvcConfiguration : WebMvcConfigurer {
     final override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler(ASSETS_PATH)
                 .setCachePeriod(864000)
+                .setCacheControl(null)
                 .addResourceLocations(assetsResourceLocation)
     }
 }
